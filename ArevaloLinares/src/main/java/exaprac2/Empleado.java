2f06d05b4d6c781caf40818328f596784141b24e
GersonCortez <gerson.arevalo20@itca.edu.sv>
2021-05-08 13:47:55 -0600

    Agregando los constructores y los gets y sets

--- ArevaloLinares/src/main/java/exaprac2/Empleado.java
+++ ArevaloLinares/src/main/java/exaprac2/Empleado.java
@@ -10,5 +10,41 @@ package exaprac2;
  * @author 50373
  */
 public class Empleado {
+    private String nombre;
+    private double salario;
+    private int cantidad;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, double salario, int cantidad) {
+        this.nombre = nombre;
+        this.salario = salario;
+        this.cantidad = cantidad;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    } 
     
 }
